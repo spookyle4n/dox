e1dc67f068c383c804e3e7743e5097db8992119c
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-07 11:06:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # coded by wyatt g #
 
-print('''
+print('''\033[31m
 
-    111111
-    111111
-    111111
-    111111
-    111111
-    111111
-    111111
-    111111111111
-    111111111111
-    
+010            001  011      011   110100     10110010010 00110110001
+ 101          101    111    001   011  101        001         110
+  001        110       011 100   1010101100       110         001
+   110  00  000          111    000      110      101         101
+    010 00 111          010    010         001    001         010
+     110  001          100   111            110   111         101     
+     
 ''')
 
 print('''           
@@ -24,6 +21,6 @@ print('''
 
 dox = str(input(' [?] open doxbin.org [y/n]: '))
 import webbrowser
-if dox == 'y': webbrowser.open('http://www.doxbin.com')
+if dox == 'y': webbrowser.open('http://www.doxbin.com/upload')
 
 if dox == 'n': exit()
