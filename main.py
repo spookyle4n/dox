4e2598169747947536917915a507cac554cdb81c
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-11 10:53:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coded by wyatt g #
 
 import webbrowser
+import time
 
 print('''
 
@@ -17,26 +18,6 @@ print('''
 
 \033[31m 	  tiktok:  @jahsehrare
 \033[31m 	  instagram: @spookyle4n
-''')
-
-first_name = input("First Name: ")
-
-last_name = input("Last Name: ")
-
-address = input("Address: ")
-
-ip = input("ip: ")
-
-age = input("age: ")
-
-import time
-
-print('''
-\033[31m       ____            _     _       
-\033[32m      |  _ \  _____  _| |__ (_)_ __  
-\033[33m      | | | |/ _ \ \/ / '_ \| | '_ \ 
-\033[34m      | |_| | (_) >  <| |_) | | | | |
-\033[35m      |____/ \___/_/\_\_.__/|_|_| |_|
 ''')
 
 first_name = input("First Name: ")
