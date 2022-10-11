4817dd79cdb2c0c60f5fa9d2c311c6885d764ca6
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-11 10:51:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # coded by wyatt g #
 
+import webbrowser
+
 print('''
 
 \033[31m       ____            _     _       
@@ -27,6 +29,26 @@ ip = input("ip: ")
 
 age = input("age: ")
 
+import time
+
+print('''
+\033[31m       ____            _     _       
+\033[32m      |  _ \  _____  _| |__ (_)_ __  
+\033[33m      | | | |/ _ \ \/ / '_ \| | '_ \ 
+\033[34m      | |_| | (_) >  <| |_) | | | | |
+\033[35m      |____/ \___/_/\_\_.__/|_|_| |_|
+''')
+
+first_name = input("First Name: ")
+
+last_name = input("Last Name: ")
+
+address = input("Address: ")
+
+ip = input("ip: ")
+
+age = input("age: ")
+
 print("victims first name : " + " " + first_name + " " + "\n")
 time.sleep(0.5)
 
@@ -42,7 +64,7 @@ time.sleep(0.5)
 print("victims age : " + " " + age + " " + "\n")
 
 dox = str(input(' [?] open doxbin.org [y/n]: '))
-import webbrowser
+
 if dox == 'y': webbrowser.open('http://www.doxbin.com/upload')
 
 if dox == 'n': exit()
