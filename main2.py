5daf4ca3a6172c50d94e6d421f4cf1b2b9962afd
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-17 08:00:48 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -11,8 +11,8 @@ print('''
 \033[34m      | |_| | (_) >  <| |_) | | | | |
 \033[35m      |____/ \___/_/\_\_.__/|_|_| |_| 
 
-\033[31m 	        tiktok:    @jahsehrare
-\033[31m 	        instagram: @spookyle4n
+\033[31m 	    tiktok:    @jahsehrare
+\033[31m 	    instagram: @spookyle4n
 
 ''')
 print("[!] please make an account on \"doxbin.com\"")
